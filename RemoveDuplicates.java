dc593c5581e57709f735ded8a2dbaa67740d4b64
Nate Flexer <90048404+nateflexer@users.noreply.github.com>
2023-02-01 14:02:13 -0500

    Add files via upload

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 public class RemoveDuplicates {
 
-   static String removeDups(char word[], int n){
+   static String removeDups(char s[], int n){
       int index = 0;
       int j;
       for (int i = 0; i < n; i++)
       {
          for (j = 0; j < i; j++)
          {
-            if (word[i] == word[j])
+            if (s[i] == s[j])
             {
                break;
             }
@@ -17,17 +17,17 @@ public class RemoveDuplicates {
         
          if (i==j)
          {
-            word[index++] = word[i];
+            s[index++] = s[i];
          }
       }
-      return String.valueOf(Arrays.copyOf(word, index));
+      return String.valueOf(Arrays.copyOf(s, index));
    }
-   
    public static void main(String[] args) {
-      char word[] = "xyyxzx".toCharArray();
-      int n = word.length;
-      System.out.println(removeDups(word, n));
-   
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter a string of lowercase letters: ");
+      char s[]=sc.next().toCharArray();
+     
+      int n = s.length;
+      System.out.println(removeDups(s, n));
    }
- 
 }
